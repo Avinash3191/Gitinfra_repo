f89c858381848ab61290f511c8f002e2720b55e9
AVinash Yadav <avinash.miet2008@gmail.com>
2025-07-27 23:46:25 +0530

    1 stg added

--- rg.tf
+++ rg.tf
@@ -19,3 +19,13 @@ resource "azurerm_resource_group" "example" {
     environment = "Production"
   }
 }
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
